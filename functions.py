f54c2befbdad56211166f0cec55863dab0919f6d
BenitzCoding <benitzcoding@yahoo.com>
2022-05-14 13:52:02 +0800

    Added `sync_slash_commands` function to functions file.

--- functions.py
+++ functions.py
@@ -1,11 +1,16 @@
 import os
 
 from typing import Any
+from cool_utils import Terminal
 from dotenv import find_dotenv, load_dotenv
 
+async def sync_slash_commands(self):
+	await self.tree.sync()
+	Terminal.display("Application synced successfully.")
+
 def initialise_env() -> None:
-    load_dotenv(find_dotenv())
-    return None
+	load_dotenv(find_dotenv())
+	return None
 
 def get_env(constant: str) -> Any:
-    os.getenv(constant)
+	os.getenv(constant)
