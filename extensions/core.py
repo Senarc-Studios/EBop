f1f81272675de18190d2a5cf5f0311a641a01ac0
BenitzCoding <benitzcoding@yahoo.com>
2022-05-14 21:38:50 +0800

    Added unload extension command.

--- extensions/core.py
+++ extensions/core.py
@@ -5,7 +5,7 @@ from discord import app_command, Object
 from discord.ext.commands import Bot
 from discord.ext.commands import Cog
 
-from functions import User, get_env, display_error
+from functions import Extensions, User, get_env, display_error
 
 CORE_GUILD = Object(id = get_env("CORE_GUILD"))
 
@@ -27,8 +27,7 @@ class Core(Cog):
 		try:
 			await self.bot.load_extension(f"extensions.{extension}")
 			Terminal.display(f"Extension \"%yellow%{extension}%r%\" Loaded.")
-			(extension)
-			self.bot.UNLOADED_EXTENSIONS.remove(extension)
+			Extensions.register_loaded_extension(extension)
 			await interaction.response.send_message(
 				f":white_check_mark: Loaded Extension `extensions.{extension}`",
 				ephemeral=True
@@ -38,6 +37,33 @@ class Core(Cog):
 			display_error(error)
 			await interaction.response.send_message(
 				f":fire: An error occured while loading \"{extension}\" Extension:\n```py\n{error.__traceback__}\n```",
+				ephemeral=True
+			)
+
+	@app_command(
+		name = "unload",
+		guild = CORE_GUILD
+	)
+	async def unload(self, interaction, extension: str):
+		user = User(interaction.auther.id)
+		if not user.is_owner:
+			return await interaction.response.send_message(
+				":warning: It seems like you're not authorised to use this command.",
+				ephemeral=True
+			)
+		try:
+			await self.bot.unload_extension(f"extensions.{extension}")
+			Terminal.display(f"Extension \"%yellow%{extension}%r%\" Unloaded.")
+			Extensions.register_unloaded_extension(extension)
+			await interaction.response.send_message(
+				f":white_check_mark: Loaded Extension `extensions.{extension}`",
+				ephemeral=True
+			)
+		except Exception as error:
+			Terminal.error(f"An error occured while loading \"%yellow%{extension}%r%\" extension.")
+			display_error(error)
+			await interaction.response.send_message(
+				f":fire: An error occured while unloading \"{extension}\" Extension:\n```py\n{error.__traceback__}\n```",
 				ephemeral=True
 			)
 
