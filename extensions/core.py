417d8f9a83424fb3431c0d481c6097cbe5684097
BenitzCoding <benitzcoding@yahoo.com>
2022-05-14 21:45:54 +0800

    Added reload extension commnad.

--- extensions/core.py
+++ extensions/core.py
@@ -33,7 +33,7 @@ class Core(Cog):
 				ephemeral=True
 			)
 		except Exception as error:
-			Terminal.error(f"An error occured while loading \"%yellow%{extension}%r%\" extension.")
+			Terminal.error(f"An error occured while loading \"%yellow%{extension}.py%r%\" extension.")
 			display_error(error)
 			await interaction.response.send_message(
 				f":fire: An error occured while loading \"{extension}\" Extension:\n```py\n{error.__traceback__}\n```",
@@ -53,17 +53,45 @@ class Core(Cog):
 			)
 		try:
 			await self.bot.unload_extension(f"extensions.{extension}")
-			Terminal.display(f"Extension \"%yellow%{extension}%r%\" Unloaded.")
+			Terminal.display(f"Extension \"%yellow%{extension}.py%r%\" Unloaded.")
 			Extensions.register_unloaded_extension(extension)
 			await interaction.response.send_message(
 				f":white_check_mark: Loaded Extension `extensions.{extension}`",
 				ephemeral=True
 			)
 		except Exception as error:
-			Terminal.error(f"An error occured while loading \"%yellow%{extension}%r%\" extension.")
+			Terminal.error(f"An error occured while loading \"%yellow%{extension}.py%r%\" extension.")
 			display_error(error)
 			await interaction.response.send_message(
 				f":fire: An error occured while unloading \"{extension}\" Extension:\n```py\n{error.__traceback__}\n```",
+				ephemeral=True
+			)
+
+	@app_command(
+		name = "reload",
+		guild = CORE_GUILD
+	)
+	async def reload(self, interaction, extension: str):
+		user = User(interaction.auther.id)
+		if not user.is_owner:
+			return await interaction.response.send_message(
+				":warning: It seems like you're not authorised to use this command.",
+				ephemeral=True
+			)
+		try:
+			await self.bot.unload_extension(f"extensions.{extension}")
+			await self.bot.load_extension(f"extensions.{extension}")
+			Terminal.display(f"Extension \"%yellow%{extension}.py%r%\" Reloaded.")
+			Extensions.register_loaded_extension(extension)
+			await interaction.response.send_message(
+				f":white_check_mark: Reloaded Extension `extensions.{extension}`",
+				ephemeral=True
+			)
+		except Exception as error:
+			Terminal.error(f"An error occured while reloading \"%yellow%{extension}.py%r%\" extension.")
+			display_error(error)
+			await interaction.response.send_message(
+				f":fire: An error occured while reloading \"{extension}\" Extension:\n```py\n{error.__traceback__}\n```",
 				ephemeral=True
 			)
 
