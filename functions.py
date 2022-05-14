73095a1fa46992ed375fef39c116d249b1b19c33
BenitzCoding <benitzcoding@yahoo.com>
2022-05-14 21:07:56 +0800

    Added Extensions to functions file.

--- functions.py
+++ functions.py
@@ -1,9 +1,51 @@
 import os
 import traceback
 
-from typing import Any
+from discord.app_commands import Choice
+
+from typing import Any, Union
 from cool_utils import Terminal
 from dotenv import find_dotenv, load_dotenv
+
+extensions = {
+	"EXTENSIONS": [],
+	"LOADED_EXTENSIONS": [],
+	"UNLOADED_EXTENSIONS": []
+}
+
+class Extensions:
+	def register_extensions(extension: Union[list, str]) -> None:
+		if isinstance(extension, list):
+			extensions.update(
+				{
+					"EXTENSIONS": extensions.get("EXTENSIONS").extend(extension)
+				}
+			)
+		
+		elif isinstance(extension, str):
+			extensions.update(
+				{
+					"EXTENSIONS": extensions.get("EXTENSIONS").append(extension)
+				}
+			)
+
+		return None
+
+	async def get_unloaded_extensions(interaction, current: str) -> list:
+		if extensions.get("UNLOADED_EXTENSIONS") == []:
+			return [Choice(name="No Extensions", value="No Extensions")]
+		return [
+			Choice(name=extension, value=extension)
+			for extension in extensions.get("UNLOADED_EXTENSIONS") if current.lower() in extensions.lower()
+		]
+
+	async def get_loaded_extensions(interaction, current: str) -> list:
+		if extensions.get("LOADED_EXTENSIONS") == []:
+			return [Choice(name="No Extensions", value="No Extensions")]
+		return [
+			Choice(name=extension, value=extension)
+			for extension in extensions.get("LOADED_EXTENSIONS") if current.lower() in extensions.lower()
+		]
 
 async def sync_slash_commands(self) -> None:
 	await self.tree.sync()
